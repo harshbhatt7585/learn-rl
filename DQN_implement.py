5d98503ddcbcb7ca24dda76421d3dee6b330f9b3
harshbhatt7585 <harshbhatt7585@gmail.com>
2025-02-26 21:23:17 +0530

    Build DQN Policy Net

--- DQN_implement.py
+++ DQN_implement.py
@@ -47,3 +47,45 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
     
+
+
+class DQN(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+
+
+class Hyperparameters:
+    BATCH_SIZE = 128
+    GAAMA = 0.99
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 1000
+    TAU = 0.005
+    LR = 1e-4
+
+
+n_actions = env.action_space.n
+state, _ = env.reset()
+n_observations = len(state)
+config = Hyperparameters
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=config.LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+steps_done = 0
