d3a2dbf7ae68296d8f5e2d8481a3bd89c83d1404
Harsh Bhatt <harshbhatt7585@gmail.com>
2025-08-31 00:22:13 +0530

    Fix bugs

--- fundamentals/basic_environment.py
+++ fundamentals/basic_environment.py
@@ -2,7 +2,7 @@ class BasicGridWorld:
     def __init__(self, width=4, height=4):
         self.size = width * height
         self.state = (0, 0)
-        self.goal = (self.size-1, self.size-1)
+        self.goal = self.size - 1
         self.actions = {0: 'UP', 1: 'DOWN', 2: 'LEFT', 3: 'RIGHT'}
         self.current_place = 0
         self.width = width
@@ -11,38 +11,46 @@ class BasicGridWorld:
     def reset(self):
         self.state = (0, 0)
         return self.state
-    
     def step(self, action):
-        if action == 0:
-            self.current_place = self.current_place - self.width
-        elif action == 1:
-            self.current_place = self.current_place + self.width
-        elif action == 2:
-            self.current_place = self.current_place - 1
-        elif action == 3:
-            self.current_place = self.current_place + 1
+        last_place = self.current_place
+        if action == 0:  # UP
+            new_place = self.current_place - self.width
+        elif action == 1:  # DOWN
+            new_place = self.current_place + self.width
+        elif action == 2:  # LEFT
+            new_place = self.current_place - 1
+        elif action == 3:  # RIGHT
+            new_place = self.current_place + 1
         
-        if self.current_place < 0 or self.current_place >= self.size:
-            reward = -1 * 0.1
-            new_state = self.state
+        # Check for valid moves (within bounds and not crossing row boundaries for left/right)
+        valid_move = True
+        if new_place < 0 or new_place >= self.size:
+            valid_move = False
+        elif action == 2 and self.current_place % self.width == 0:  # LEFT at left edge
+            valid_move = False
+        elif action == 3 and self.current_place % self.width == self.width - 1:  # RIGHT at right edge
+            valid_move = False
+        
+        if not valid_move:
+            reward = -0.1
+            new_state = self.current_place
         else:
+            self.current_place = new_place
             # give rewards based on how close the goal is than the last place
             current_distance_to_goal = abs(self.current_place - self.goal)
-            last_distance_to_goal = abs(self.state - self.goal)
+            last_distance_to_goal = abs(last_place - self.goal)
             reward = last_distance_to_goal - current_distance_to_goal
             new_state = self.current_place
         
         self.state = new_state
         done = self.state == self.goal
 
-
         return new_state, reward, done
- 
 
     
 
 if __name__ == "__main__":
-    env = GridWorld()
+    env = BasicGridWorld()
     env.reset()
     env.step(0)
     env.step(1)
