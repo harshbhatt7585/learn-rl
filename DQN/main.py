f695fe007fa23d77f4237ab06c5cd3826a946e8f
Harsh Bhatt <harshbhatt7585@gmail.com>
2025-11-06 04:11:02 +0530

    full training loop

--- DQN/main.py
+++ DQN/main.py
@@ -1,5 +1,3 @@
-from collections import deque
-from traceback import print_tb
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -22,7 +20,7 @@ class DQN(nn.Module):
 
     def forward(self, x):
         x = self.encoder(x)
-        x = x.view(32*9*9)
+        x = x.view(-1, 32*9*9)
         x = self.hidden_layer(x)
         x = self.action_head(x)
         return x
@@ -33,7 +31,7 @@ class DQN(nn.Module):
 class ReplayBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.buffer = deque(maxlen=capacity)
+        self.buffer = []
     
     def push(self, state, action, reward, next_state, done):
         self.buffer.append((state, action, reward, next_state, done))
@@ -57,8 +55,12 @@ def train():
     num_episodes = 100
     max_steps = 100
     epsilon = 0.2
+    batch_size = 32
+    gamma = 0.99
+    
     
     dqn = DQN(num_actions=env.action_space.n)
+    optimizer = optim.Adam(dqn.parameters(), lr=0.001)
     
     for episode in range(num_episodes):
         # initialize sequence
@@ -68,16 +70,53 @@ def train():
         for t in range(max_steps):
             if random.random() < epsilon:
                 action = env.action_space.sample()
-                print(action)
             else:
-                action = dqn(state)
-                # print(action.shape)
+                with torch.no_grad():
+                    action = dqn(state)
 
                 # convert action logits to probs
-                # action_probs = nn.Softmax()(action)
-                # print(action_probs.shape)
-                # action = action_probs.argmax()
-                # print(action)
+                action_probs = nn.Softmax()(action)
+                action = action_probs.argmax().item()
+
+            next_state, reward, terminated, truncated, info = env.step(action)
+            next_state = process_state(next_state)
+            done = terminated or truncated
+            replay_buffer.push(state.numpy(), action, reward, next_state.numpy(), done)
+            state = next_state
+        
+
+            if len(replay_buffer) > batch_size:
+                batch = replay_buffer.sample(batch_size)
+                s, a, r, s_, d = zip(*batch)
+
+
+                s = torch.tensor(s)
+                a = torch.tensor(a)
+                r = torch.tensor(r)
+                s_ = torch.tensor(s_)
+                d = torch.tensor(d)
+
+                q_values = dqn(s).gather(1, a.unsqueeze(1))
+                next_q_value = dqn(s_).max(dim=1).values    
+                
+                expected_q = r + gamma * next_q_value 
+
+                loss = F.mse_loss(q_values, expected_q.detach())
+
+                print(loss)
+
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+        
+            if done:
+                break
+
+    
+        
+            
 
 
 
@@ -93,7 +132,7 @@ if __name__ == "__main__":
     from ale_py import ALEInterface
     ale = ALEInterface()
 
-    env = gym.make("ALE/Assault-v5")
+    env = gym.make("ALE/Assault-v5", render_mode="human") 
     # state, _ = env.reset()
 
     # # convert to grayscale
